66766ef36cf978074ed59681c6663482f423bf3f
dkpat <dkpat@LAPTOP-H2TQ9942.wifi.uc.edu>
2022-10-25 14:33:26 -0400

    First Commit

---
+++ Thorin_Assignment09/ClassPackage/TrainClass.py
@@ -0,0 +1,13 @@
+'''
+Name: David Patton
+email: pattondk@mail.uc.edu
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: I created a class for our group project to model a train 
+transportation scenario 
+Citations: David Patton
+Anything else that's relevant: This is the module holding the train class 
+also this was fun!!! :) 
+'''
+print("main")
---
+++ Thorin_Assignment09/MainPackage/Main.py
@@ -0,0 +1,10 @@
+'''
+Name: Mia N
+email: 
+Assignment: Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description:   
+Citations: 
+Anything else that's relevant: This is the module holding the main
+'''
