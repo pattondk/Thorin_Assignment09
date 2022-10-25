c21304fc946e9166bf0e36088e032df182832ddd
Roger <Roger@DESKTOP-9QS0OM9.wifi.uc.edu>
2022-10-25 14:41:21 -0400

    Documenting

--- Thorin_Assignment09/ClassPackage/CarClass.py
+++ Thorin_Assignment09/ClassPackage/CarClass.py
@@ -4,7 +4,7 @@ email: poduskrd@mail.uc.edu
 Assignment: Thorin_Assignment 09
 Course: IS 4010
 Semester/Year: Fall 2022
-Brief Description: Utilizes AirplaneClass to model a real world object
+Brief Description: Roger's class to add to Thorin project; Models a car in real world
 Citations: N/a
 Anything else that's relevant: N/a
 '''
