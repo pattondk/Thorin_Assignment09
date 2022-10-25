5778924276bf4f86ff40db2e71b36463f7738490
Roger <Roger@DESKTOP-9QS0OM9.wifi.uc.edu>
2022-10-25 14:40:29 -0400

    Adding car class

---
+++ Thorin_Assignment09/ClassPackage/CarClass.py
@@ -0,0 +1,10 @@
+'''
+Name: Roger Poduska
+email: poduskrd@mail.uc.edu
+Assignment: Thorin_Assignment 09
+Course: IS 4010
+Semester/Year: Fall 2022
+Brief Description: Utilizes AirplaneClass to model a real world object
+Citations: N/a
+Anything else that's relevant: N/a
+'''
