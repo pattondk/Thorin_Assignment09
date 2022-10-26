ff8df5b029a489fc3ecd14958a20a048f6e090cc
pattondk <116082224+pattondk@users.noreply.github.com>
2022-10-25 20:09:13 -0400

    Updated entire TrainClass due to error fixed error

--- Thorin_Assignment09/ClassPackage/TrainClass.py
+++ Thorin_Assignment09/ClassPackage/TrainClass.py
@@ -15,9 +15,22 @@ class Train():
         self.NumTrainCars = NumTrainCars
         self.validateGuestCapacity(GuestCapacity)
         self.BlowWhistle = False 
+    
+    def __repr__(self):
+        return "There are " + self.NumTrainCars
+    
+    def __str__(self):
+        return ("The number of cars in the train is " + str(self.NumTrainCars) + " And the number of people on the train is " 
+                + str(self.GuestCapacity))
        
     def getGuestCapacity(self):
         return self.GuestCapacity
+    
+    def getNumTrainCars(self):
+        return self.NumTrainCars
+    
+    def getBlowWhistle(self):
+        return self.BlowWhistle
         
     def setGuestCapacity(self, GuestCapacity):
         self.validateGuestCapacity(GuestCapacity)
@@ -30,26 +43,14 @@ class Train():
     
     def setNumTrainCars (self, NumTrainCars):
         if NumTrainCars >= 15:
-            print("The train has to many cars!")
+            print("The train has too many cars!")
         else:
             self.NumTrainCars = NumTrainCars
     
-    def getNumTrainCars(self):
-        return self.NumTrainCars
+    def setBlowWhistle(self, status):
+        self.BlowWhistle = status
+        return "The train horn status has changed."
     
-    def setBlowWhistle(self): 
+    def WhistleOn(self): 
         self.BlowWhistle = True 
-        return("The whistle is blowing")
-        
-        self.BlowWhistle = False
-        print("You blew the whistle!")
-        
-    def getBlowWhistle(self):
-        return self.BlowWhistle
-       
-    def __repr__(self):
-        return "There are  " + self.NumTrainCars
-    
-    def __str__(self):
-        return ("Wedding Date is = " + str(self.NumTrainCars) + " And the number of people attending is " 
-                + str(self.GuestCapacity))
+        return("The whistle is blowing toot toot please move")
